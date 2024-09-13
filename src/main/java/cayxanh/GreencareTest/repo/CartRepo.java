97ddbf97660b47db6407ebd9b2587fa49a34ae55
TannedDolphin <121384092+TannedDolphin@users.noreply.github.com>
2024-09-13 21:48:52 +0700

    Delete src directory

--- src/main/java/cayxanh/GreencareTest/repo/CartRepo.java
+++
@@ -1,9 +0,0 @@
-package cayxanh.GreencareTest.repo;
-
-import cayxanh.GreencareTest.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartRepo extends JpaRepository<Cart, Integer> {
-}
--- src/main/java/cayxanh/GreencareTest/repo/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package cayxanh.GreencareTest.repo;
-
-import cayxanh.GreencareTest.entity.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepo extends JpaRepository<Orders, Integer> {
-}
